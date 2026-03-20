aead31e1d5c22b9a0f52ade0ac19a15dd2838b35
casper80833-netizen <casper80833@gmail.com>
2026-03-20 20:59:05 +0200

    ביטול תנועה אין סופית
    מאפשר תזוזה רק אם השחקן עוזב את הלחצן

--- Assets/Scripts/PlayerTopPart.cs
+++ Assets/Scripts/PlayerTopPart.cs
@@ -7,31 +7,45 @@ public class PlayerTopPart : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform movePoint;
     public LayerMask whatStopsMovement;
+    private bool isInputPressed = false;
 
     void Start()
     {
         movePoint.parent = null;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
+
+            if (moveX == 0f && moveY == 0f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
-                {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                }
+                isInputPressed = false;
             }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+
+            if (isInputPressed == false)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+                if (Mathf.Abs(moveX) == 1f)
                 {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    isInputPressed = true;
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(moveX, 0f, 0f), .2f, whatStopsMovement))
+                    {
+                        movePoint.position += new Vector3(moveX, 0f, 0f);
+                    }
+                }
+                else if (Mathf.Abs(moveY) == 1f)
+                {
+                    isInputPressed = true; 
+
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, moveY, 0f), .2f, whatStopsMovement))
+                    {
+                        movePoint.position += new Vector3(0f, moveY, 0f);
+                    }
                 }
             }
         }
