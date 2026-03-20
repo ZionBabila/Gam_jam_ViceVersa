47063424d5c4961fcf722ffc34b9a847edf7fbd6
casper80833-netizen <casper80833@gmail.com>
2026-03-20 21:22:44 +0200

    שינוי גודל מצלמה + יצירת בלוק

---
+++ Assets/Scripts/PlayerBottomPart.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerBottomPart : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public LayerMask whatStopsMovement;
+    private bool isInputPressed = false;
+
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            float moveX = -Input.GetAxisRaw("Horizontal");
+            float moveY = -Input.GetAxisRaw("Vertical");
+
+            if (moveX == 0f && moveY == 0f)
+            {
+                isInputPressed = false;
+            }
+
+            if (isInputPressed == false)
+            {
+                if (Mathf.Abs(moveX) == 1f)
+                {
+                    isInputPressed = true;
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(moveX, 0f, 0f), .2f, whatStopsMovement))
+                    {
+                        movePoint.position += new Vector3(moveX, 0f, 0f);
+                    }
+                }
+                else if (Mathf.Abs(moveY) == 1f)
+                {
+                    isInputPressed = true;
+
+                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, moveY, 0f), .2f, whatStopsMovement))
+                    {
+                        movePoint.position += new Vector3(0f, moveY, 0f);
+                    }
+                }
+            }
+        }
+    }
+}
+
