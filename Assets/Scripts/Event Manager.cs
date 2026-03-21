bee998c50e7754f7440b485f5fb14bbe14d24a69
casper80833-netizen <casper80833@gmail.com>
2026-03-21 21:05:42 +0200

    ui של הלבבות

--- Assets/Scripts/Event Manager.cs
+++ Assets/Scripts/Event Manager.cs
@@ -5,19 +5,44 @@ using System.Collections;
 public class EventManager : MonoBehaviour
 {
     public static int Harts = 3;
+    public GameObject Hart1;
+    public GameObject Hart2;
+    public GameObject Hart3;
+
 
     void Start()
     {
         Harts = 3;
     }
+
+    // Restart System
     void Update()
     {
-        if (Harts<0)
+        HartUi();
+
+        if (Harts<1)
         {
             StartCoroutine(WaitAndReload());
         }
     }
 
+    void HartUi()
+    {
+        if (Harts < 3)
+        {
+            Destroy(Hart1);
+        }
+
+        if (Harts < 2)
+        {
+            Destroy(Hart2);
+        }
+
+        if (Harts < 1)
+        {
+            Destroy(Hart3);
+        }
+    }
     private IEnumerator WaitAndReload()
     {
         PlayerTopPart.isDead = true;
