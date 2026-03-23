efad12b8e3dfdcd2616d62bd5aea9691e6d780ae
casper80833-netizen <casper80833@gmail.com>
2026-03-23 18:45:30 +0200

    ui work

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,9 +1,39 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MenuController : MonoBehaviour
 {
-    public void OnButtonClick()
+    public GameObject Menu;
+
+    void Start()
     {
-        Debug.Log("ui work!");
+        Menu.SetActive(false);
+    }
+    public void OnSettingsClick()
+    {
+        Debug.Log("settings work!");
+        Menu.SetActive(true);
+    }
+
+    public void OnCloseClick()
+    {
+        Debug.Log("close work!");
+        Menu.SetActive(false);
+    }
+
+    public void OnRestartClick()
+    {
+        Debug.Log("restart work!");
+        StartCoroutine(WaitAndReload());
+    }
+
+    private IEnumerator WaitAndReload()
+    {
+        PlayerTopPart.isDead = true;
+        PlayerBottomPart.isDead = true;
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
